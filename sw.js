15fd20d108fcedb93bd78592ca7e85ab45906f0d
osebee-tech <oseabee@gmail.com>
2026-09-18 22:30:28 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,27 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('asset-store').then((cache) => cache.addAll(['./index.html', './manifest.json']))
+const CACHE='asset-store-v2';
+const CORE=['./index.html','./manifest.json'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached=>{
+      if(cached) return cached;
+      return fetch(event.request).then(response=>{
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});
+        return response;
+      }).catch(()=>cached);
+    })
   );
 });
